76c1366acbb158a80424d26c80d5ecc562fdd517
Andrei Sima <92935858+randalthor42@users.noreply.github.com>
2025-08-02 00:16:40 +0200

    init commit

---
+++ src/main/java/dev/andreisima/notification_scheduler/NotificationSchedulerApplication.java
@@ -0,0 +1,13 @@
+package dev.andreisima.notification_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/andreisima/notification_scheduler/NotificationSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andreisima.notification_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
